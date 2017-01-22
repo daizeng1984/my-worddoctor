6caaf5c4e470ced3d29ed21ab590b5a0fa733d71
daizeng1984 <daizeng1984@gmail.com>
2017-01-22 22:44:57 +0800

    - Added first 'working' whd logic

--- rplugin/python3/deoplete/sources/deoplete_whd.py
+++ rplugin/python3/deoplete/sources/deoplete_whd.py
@@ -21,19 +21,40 @@ class Source(Base):
         m = re.search(r'\w*$', context['input'])
         return m.start() if m else -1
 
+    def _get_words_from_file(self, filename):
+        with open (filename) as f:
+            words = [re.findall(r"[\w']+", line) for line in f]
+            return [w for sub in words for w in sub]
+
     #Profile?
     #@profiler.profile
     def gather_candidates(self, context):
-        # Dummy output
-        result = []
-        result.append( {
-            'word': 'First Word Hints!'
-            }
-            )
-        result.append( {
-            'word': 'Second Word Hints!'
-            }
-            )
-        return result
-        
+        ORDER = 2
+        DELIMITER = '+'
+        # if you own a whd degree, you should be able to know:
+        whdKnowledgeResource='/tmp/obama08.txt' #TODO ask user to set it
 
+        # Dummy output
+        words = self._get_words_from_file(whdKnowledgeResource)
+        lowercaseWords = [x.lower() for x in words]
+
+        # Build dictionary for whd knowledge
+        knowledge = {}
+        for i in range(ORDER, len(lowercaseWords)) :
+            key = DELIMITER.join(lowercaseWords[i-ORDER:i])
+            knowledge.setdefault(key, set([]))
+            knowledge[key].add(lowercaseWords[i])
+
+        # get the most recent two words (lowercase)
+        inputLine = context['input']
+        inputWords = [x.lower() for x in re.findall(r"[\w']+", inputLine)]
+
+        while (len(inputWords) < ORDER) :
+            inputWords.append('')
+
+        inputKey=DELIMITER.join(inputWords[-ORDER:])
+
+        hints = knowledge.setdefault(inputKey, [])
+
+        result = [ {'word':x} for x in hints ]
+        return result
